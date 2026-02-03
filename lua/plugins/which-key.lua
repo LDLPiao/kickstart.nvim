aa5f47a9df5923f93fc7532c5613f9f429b36def
ldlpiao <luiz.l.piao@gmail.com>
2026-02-03 01:18:26 -0300

    adiciona Harpoon2 aos plugins

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -48,6 +48,7 @@ return {
         { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>l', group = 'Harpoon [L]ist', mode = { 'n' } },
       },
     },
   },
